81c8c87f2049b02f2794b5f11bd02fc72a67c15e
“LePhuc” <lephuc@Les-MacBook-Pro.local>
2017-07-07 14:23:31 +0700

    Notification 2

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,7 +1,6 @@
 /*global App:true*/
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
-    $('#messages').removeClass('hidden');
     return $('#messages').append(this.renderMessage(data));
   },
 
